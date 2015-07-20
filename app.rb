481529ea77c44361386a2e78af00b784c793a118
Nicholas Emanuel <njemanuel01@gmail.com>
2015-07-20 12:25:02 -0500

    Added database connection to app.rb

---
+++ app.rb
@@ -0,0 +1,36 @@
+require "pry"
+require "active_record"
+require "sinatra"
+require "sinatra/reloader"
+
+configure :development do
+  require 'sqlite3'
+  ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'stories.db')
+end
+
+## Sinatra specific 
+configure :production do
+  require 'pg'
+  db = URI.parse(ENV['DATABASE_URL'])
+
+  ActiveRecord::Base.establish_connection(
+    :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
+  )
+end
+
+# So that ActiveRecord explains the SQL it's running in the logs.
+ActiveRecord::Base.logger = ActiveSupport::Logger.new(STDOUT)
+
+unless ActiveRecord::Base.connection.table_exists?(:users)
+  ActiveRecord::Base.connection.create_table :users do |t|
+    t.text :email
+    t.text :password
+end  
+
+
+
