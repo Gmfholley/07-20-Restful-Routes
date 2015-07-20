32624b5164b5c1ffbb4d9b321de13eeb4a6b3b1d
Gwendolyn Holley <gmfholley@gmail.com>
2015-07-20 15:09:49 -0500

    Adds valid_password? method to model

--- models/user.rb
+++ models/user.rb
@@ -1,6 +1,14 @@
 class User < ActiveRecord::Base
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: true
   validates :password, presence: true
   has_many :stories
+  
+  
+  def valid_password?(password)
+    if !(BCrypt::Password.new(self.password) == password)
+      errors.messages["login"] = "Login Failed."
+    end
+    return errors.messages.empty?
+  end
   
 end 
